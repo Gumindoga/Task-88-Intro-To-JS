283b7f63fd0a11554035f2fd3d5ccaf3f0960a53
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2022-06-25 16:09:21 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let studName = "Blessed B Muvindi";
+let studHeight = "1.82 m";
+let studCountry = "Zimbabwe";
+document.write("My name is ", studName);
+document.write(". I am ", studHeight, " tall.");
+document.write(" I am from ", studCountry, ".");
